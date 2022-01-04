fec479622e450584f2230ba57bbd3ea246917cac
Christian <cespinoza8001@gmail.com>
2022-01-04 13:05:18 -0500

    added the rest of the the listing info on listing cards

--- client/src/ListingCard.js
+++ client/src/ListingCard.js
@@ -2,7 +2,7 @@
 import styled from "styled-components";
 
 
-function ListingCard({ listings: { address, house_img, rent } }) {
+function ListingCard({ listings: { address, house_img, rent, city, state, square_feet, num_beds, num_baths } }) {
   return (
     <ListingStyle>
     <div>
@@ -10,8 +10,10 @@ function ListingCard({ listings: { address, house_img, rent } }) {
         <img
           src={house_img} alt='houseimage' width="500" height="400"/>
         <div>
-          <h4>{address}</h4>
-          <h5>Rent: {rent}</h5>
+          <h4>{address}, {city}, {state}</h4>
+          <h4>Sq. ft: {square_feet}</h4>
+          <h4>Bed/Bath: {num_beds}/{num_baths}</h4>
+          <h5>Rent: ${rent}/month</h5>
         </div>
       </div>
       <p>♡</p>
@@ -23,13 +25,13 @@ function ListingCard({ listings: { address, house_img, rent } }) {
 export default ListingCard;
 
 const ListingStyle = styled.div`
-  border: 3px solid black;
+  border: 4px solid black;
   position: relative;
-  margin: 15px;
+  margin: 20px;
   margin-left: 30%;
   margin-right: 30%;
-  border-radius: 70px;
+  border-radius: 10px;
   padding: 7px;
-  box-shadow: 10px 10px grey;
+  box-shadow: 10px 10px lightblue;
   background: white;
 `;
