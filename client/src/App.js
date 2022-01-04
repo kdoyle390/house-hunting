0a8ec1d1c719404d2dc43aa7405a472d4d819deb
Christian <cespinoza8001@gmail.com>
2022-01-04 12:33:00 -0500

    added some styling to listing card and rendered all listings

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
-
 import './App.css';
 import ListingContainer from './ListingContainer';
 
 function App() {
 
 
-  const [listings, setListings] = useState([])
+  // const [listings, setListings] = useState([])
 
-  useEffect( () => {
-    fetch(`http://localhost:6001/results`)
-      .then(resp => resp.json())
-      .then(data => setListings(data));
-  },[])
+  // useEffect( () => {
+  //   fetch(`http://localhost:6001/results`)
+  //     .then(resp => resp.json())
+  //     .then(data => setListings(data));
+  // },[])
 
 
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
-      <Header/>
-      <ListingContainer listings={listings} setListings={setListings}/>
+      {/* <ListingContainer listings={listings} setListings={setListings}/> */}
+      <ListingContainer/>
     </div>
   );
 }
--- client/src/ListingContainer.js
+++ client/src/ListingContainer.js
@@ -1,16 +1,56 @@
-function ListingContainer () {
+import React, { useState, useEffect } from 'react';
+import ListingCard from "./ListingCard"
+
+
+function ListingContainer(){
 
     const [listings, setListings] = useState([])
 
-    useEffect( () => {
-      fetch(`http://localhost:6001/results`)
-        .then(resp => resp.json())
-        .then(data => setListings(data));
-    },[])
 
-    return(
-        <ListingCard listings={listings} setListings={setListings}/>
+    useEffect(() => {
+        fetch(`http://localhost:6001/houses`)
+        .then(resp => resp.json())
+        .then((listings) => {
+            setListings(listings)
+        })
+    }, [])
+
+    // const filteredListings = listings.filter(listing => {
+    //     return listing.//(whatever we want to search by).toLowerCase().includes(search.toLowerCase())
+    // });
+
+
+    const renderListings = listings.map(listings => {
+        return <ListingCard key={listings.id} listings={listings}/>
+    })
+
+    return (
+        <div>
+            {renderListings}
+        </div>
     )
 }
 
 export default ListingContainer;
+
+
+
+
+
+
+// function ListingContainer () {
+
+//     const [listings, setListings] = useState([])
+
+//     useEffect( () => {
+//       fetch(`http://localhost:6001/results`)
+//         .then(resp => resp.json())
+//         .then(data => setListings(data));
+//     },[])
+
+//     return(
+//         <ListingCard listings={listings} setListings={setListings}/>
+//     )
+// }
+
+// export default ListingContainer;
